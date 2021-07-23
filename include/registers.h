571ff51bc6678f24801c7ecf0e79639f5f0ac40c
Dalton Hensley <daltonhensley2000@gmail.com>
2021-07-23 16:48:48 -0400

    [Fix]: add an unused register to make FlagBitField more accurate.

--- include/registers.h
+++ include/registers.h
@@ -7,26 +7,29 @@ struct FlagBitField
 {
   Byte C : 1; // Carry Flag
   Byte N : 1; // Add/Subtract Flag
+
   // Parity/Overflow Flag
   union
   {
     Byte P : 1;
     Byte V : 1;
   };
-  Byte X : 1; // Not Used
-  Byte H : 1; // Half Carry Flag
-  Byte Z : 1; // Zero Flag
-  Byte S : 1; // Sign Flag
+
+  Byte X1 : 1; // Not Used
+  Byte H  : 1; // Half Carry Flag
+  Byte Z  : 1; // Zero Flag
+  Byte S  : 1; // Sign Flag
+  Byte X2 : 1; // Not Used
 };
 
 struct ByteRegister
 {
 
   // Main register set
- static  Byte A_Reg_A;
- static  Byte B_Reg_A;
- static  Byte D_Reg_A;
- static  Byte H_Reg_A;
+  static Byte A_Reg_A;
+  static Byte B_Reg_A;
+  static Byte D_Reg_A;
+  static Byte H_Reg_A;
 
   static Byte F_Reg_A;
   static Byte C_Reg_A;
