d2a2dd0a0d300983815ba700ff97dea540ea62aa
Dalton Hensley <daltonhensley2000@gmail.com>
2021-07-29 21:05:36 -0400

    [Feature]: add better formatting of error code on giving argc an invaild number of args. The program now also spits -1 on exit.

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,25 @@
 #include "../include/registers.h"
 #include "../include/z80.h"
 
-
-
-#include "../include/timer.h"
 #include "../include/load_rom.h"
+#include "../include/timer.h"
 
 int main(int argc, char** argv)
 {
 
-    if(argc != 2)
-        fmt::print("Error! Invaild number of arguments. Expected a path to binary test rom.");
-    else 
-    {
-        util::load_rom(argv[1]);
-    }
-  
+  if (argc != 2)
+  {
+    fmt::print("Error! Invaild number of arguments.\nExpected a path to binary "
+               "test rom.\n");
+
+    // Spit error code.
+    return -1;
+  }
+  else
+  {
+    util::load_rom(argv[1]);
+  }
+
   Z80CPU cpu{};
 
   cpu.executeInstruction();
