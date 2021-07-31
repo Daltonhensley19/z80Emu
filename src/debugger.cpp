62f80abdd94dc55e7337df24e70a9d80de8774ca
Dalton Hensley <daltonhensley2000@gmail.com>
2021-07-31 04:26:15 -0400

    Added a semicolon.

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -20,7 +20,7 @@ GLFWwindow* debug::debug_glfw_init()
 #elif defined(__APPLE__)
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
 
