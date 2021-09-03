b4bedc4464576313b1567e221b1afcc7662c05a4
Daltonhensley19 <Daltonhensley2000@gmail.com>
2021-09-03 13:20:35 -0400

    [Documentation]: added comments to timer.cpp

--- src/utility/timer.cpp
+++ src/utility/timer.cpp
@@ -1,15 +1,18 @@
 #include "../../include/timer.h"
 
+// Start elapsed time. Used for benchmarking.
 void Timer::start()
 {
- m_start_point =  m_stopwatch.now();
+  m_start_point = m_stopwatch.now();
 }
 
+// Stop elapsed time. Used for benchmarking.
 void Timer::stop()
 {
- m_stop_point = m_stopwatch.now();
+  m_stop_point = m_stopwatch.now();
 }
 
+// Calculates total elapsed time. (total_time = time_end - time_start)
 void Timer::timer_duration()
 {
   auto diff = std::chrono::duration<double>(m_stop_point - m_start_point);
