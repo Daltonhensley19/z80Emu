6567e273c0974b794badc6ada09634620bc5eaad
Daltonhensley19 <Daltonhensley2000@gmail.com>
2021-09-03 13:13:33 -0400

    [Documemtation]: added comments to stack.cpp

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,11 +1,13 @@
 #include "../include/stack.h"
 #include "../include/z80.h"
 
+// Pushes a byte onto the stack.
 void Stack::push_byte(Byte byte)
 {
   ram[--Z80CPU::sp] = byte;
 }
 
+// Pushes a word onto the stack.
 void Stack::push_word(Word word)
 {
   Byte highByte = (word >> BYTE_SHIFT_ALIGNMENT) & MAX_BYTE_SIZE;
@@ -15,6 +17,7 @@ void Stack::push_word(Word word)
   ram[--Z80CPU::sp] = lowByte;
 }
 
+// Pops a word off of the stack.
 Word Stack::pop_word()
 {
   Byte lowByte  = ram[Z80CPU::sp++];
